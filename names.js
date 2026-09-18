06a55585fbfa0fe463d0883a73158b173d635af4
Frédéric Poitevin <frederic.poitevin@gmail.com>
2026-09-18 14:18:00 -0700

    Update names.js

--- names.js
+++ names.js
@@ -2,17 +2,8 @@
 window.BABY_NAMES = [
   { id: "marcel", name: "Marcel" },
   { id: "james", name: "James" },
-  { id: "jack", name: "Jack", note: "see also Jake" },
-  { id: "jake", name: "Jake", note: "see also Jack" },
   { id: "pierre", name: "Pierre" },
   { id: "peter", name: "Peter" },
-  { id: "george", name: "George" },
-  { id: "jules", name: "Jules" },
+  { id: "georges", name: "Georges", note: "was George" },
   { id: "felix", name: "Felix" },
-  { id: "marcello", name: "Marcello", note: "related to Marcel" },
-  { id: "willie", name: "Willie" },
-  { id: "billy", name: "Billy" },
-  { id: "tom", name: "Tom" },
-  { id: "milo", name: "Milo" },
-  { id: "archie", name: "Archie" },
 ];
